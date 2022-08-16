9306631a931eb121459ea75c59eba8c52732e873
AnthonyJHizon <AnthonyJHizon714@gmail.com>
2022-08-15 17:24:22 -0700

    remove console.log

--- utils/cache/auctionHouse.js
+++ utils/cache/auctionHouse.js
@@ -3,7 +3,6 @@ import getAccessToken from '../../utils/db/getAccessToken'
 import cacheFormatAuctionHousesData from '../formatData/cache/auctionHouse'
 
 module.exports = async function AuctionHouse() {
-  console.log("fetching");
   const accessToken = await getAccessToken();
   const auctionHouseRes = await fetch(`https://us.api.blizzard.com/data/wow/connected-realm/4728/auctions/index?namespace=dynamic-classic-us&locale=en_US&access_token=${accessToken}`, {
   method: 'GET',
