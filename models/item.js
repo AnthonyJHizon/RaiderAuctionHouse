87052cab9e1b1725e32b456ae8bfeb1b62dcfcf7
AnthonyJHizon <AnthonyJHizon714@gmail.com>
2023-07-24 13:07:47 -0700

    optimization changes and add cron job to detect new items

--- models/item.js
+++ models/item.js
@@ -1,42 +1,42 @@
 const mongoose = require('mongoose');
 
 const itemSchema = new mongoose.Schema({
-  _id: Number,
-  name: {
-    type: String,
-    required: true,
-    index: true,
-  },
-  levelReq: {
-    type: Number,
-    required: true,
-  },
-  itemLevel: {
-    type: Number,
-    required: true,
-  },
-  itemClass: {
-    type: String,
-    required: true,
-    index: true,
-  },
-  itemSubclass: {
-    type: String,
-    required: true,
-  },
-  itemEquip: {
-    type: String //not required since some items do not have a itemEquip.name attribute. (Wands, Guns)
-  },
-  itemQuality: {
-    type: String,
-    required: true,
-  },
-  iconURL: {
-    type: String,
-    require: true,
-  }
-})
+	_id: Number,
+	name: {
+		type: String,
+		required: true,
+		index: true,
+	},
+	levelReq: {
+		type: Number,
+		required: true,
+	},
+	itemLevel: {
+		type: Number,
+		required: true,
+	},
+	itemClass: {
+		type: String,
+		required: true,
+		index: true,
+	},
+	itemSubclass: {
+		type: String,
+		required: true,
+	},
+	itemEquip: {
+		type: String, //not required since some items do not have a itemEquip.name attribute. (Wands, Guns)
+	},
+	itemQuality: {
+		type: String,
+		required: true,
+	},
+	iconURL: {
+		type: String,
+		require: true,
+	},
+});
 
-const Item = mongoose?.models?.Item || mongoose.model("Item", itemSchema);
+const Item = mongoose?.models?.Item || mongoose.model('Item', itemSchema);
 
 module.exports = Item;
