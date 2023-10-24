c8508c0270732620cc9fc3f4b4c3576bfcacc5c3
AnthonyJHizon <AnthonyJHizon714@gmail.com>
2023-10-24 15:06:44 -0700

    fix security risk errors

--- components/Auction.js
+++ components/Auction.js
@@ -8,21 +8,20 @@ export default function Auction({ itemId, itemName, itemIcon, itemVal }) {
 			key={itemId}
 			id={itemName}
 			className="grid grid-cols-3 w-full h-[58px] items-center bg-neutral-50 hover:bg-neutral-200"
-			href={'https://wowhead.com/wotlk/item=' + itemId}
-			target="_blank"
 		>
 			<a
 				href={'https://wowhead.com/wotlk/item=' + itemId}
+				className="justify-self-start"
 				target="_blank"
 				rel="noreferrer"
-				className="justify-self-start"
 			>
 				<Image src={itemIcon} alt="" height="58px" width="58px" />
 			</a>
 			<a
 				href={'https://wowhead.com/wotlk/item=' + itemId}
-				target="_blank"
 				className="justify-self-center"
+				target="_blank"
+				rel="noreferrer"
 			>
 				{itemName}
 			</a>
