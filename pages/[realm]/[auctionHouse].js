6c903f59c556b1e1eb20352c760247ddfd81bdfd
AnthonyJHizon <AnthonyJHizon714@gmail.com>
2023-11-20 14:27:24 -0800

    update deprecated features

--- pages/[realm]/[auctionHouse].js
+++ pages/[realm]/[auctionHouse].js
@@ -223,7 +223,7 @@ export default function Auctions({ data }) {
 				<div className="flex items-center h-[5%] justify-center text-center">
 					<h2 className="text-header-2">{filterIndicator}</h2>
 				</div>
-				<div className="h-[72.9%] w-full overflow-y-auto bg-neutral-50  scrollbar-thin scrollbar-thumb-cyan scrollbar-track-inherit">
+				<div className="h-[72.9%] w-full overflow-y-scroll bg-neutral-50  scrollbar-thin scrollbar-thumb-cyan scrollbar-track-inherit">
 					{loading ? (
 						<div className="flex items-center justify-center text-center text-header-2">
 							<p className="animate-pulse">Loading...</p>
