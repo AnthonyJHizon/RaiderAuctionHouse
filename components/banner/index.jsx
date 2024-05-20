c954032b96d47162de775e53b270db93778382d9
AnthonyJHizon <AnthonyJHizon714@gmail.com>
2024-05-20 11:12:12 -0700

    add poster to video

--- components/banner/index.jsx
+++ components/banner/index.jsx
@@ -2,9 +2,10 @@ import Description from './description';
 
 export default function Banner() {
 	return (
-		<div className="relative w-full min-h-[350px] h-3/5 flex flex-wrap justify-center">
+		<div className="relative w-full min-h-[350px] h-3/5 flex flex-wrap justify-center bg-none">
 			<video
 				className="absolute h-full w-full object-cover"
+				poster="/videos/poster.png"
 				autoPlay
 				muted
 				loop
