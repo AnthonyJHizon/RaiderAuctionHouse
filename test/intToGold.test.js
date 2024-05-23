7a10382706e96899817b4b4000e92b44bc11925b
AnthonyJHizon <AnthonyJHizon714@gmail.com>
2024-05-23 14:00:13 -0700

    change price display format

--- test/intToGold.test.js
+++ test/intToGold.test.js
@@ -3,10 +3,10 @@ import floatToGold from '../utils/formatData/props/floatToGold';
 test('convert float value to gold/silver/copper representation', () => {
 	expect(floatToGold(999.9999)).toBe('999g 99s 99c');
 	expect(floatToGold(99.9909)).toBe('99g 99s 9c');
-	expect(floatToGold(9.0909)).toBe('9g 9s 9c');
-	expect(floatToGold(0.0909)).toBe('9s 9c');
+	expect(floatToGold(9.0909)).toBe('9g 09s 09c');
+	expect(floatToGold(0.0909)).toBe('9s 09c');
 	expect(floatToGold(0.0009)).toBe('9c');
-	expect(floatToGold(99.09)).toBe('99g 9s');
-	expect(floatToGold(99.0009)).toBe('99g 9c');
-	expect(floatToGold(99)).toBe('99g');
+	expect(floatToGold(99.09)).toBe('99g 9s 00c');
+	expect(floatToGold(99.0009)).toBe('99g 00s 09c');
+	expect(floatToGold(99)).toBe('99g 00s 00c');
 });
