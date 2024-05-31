794e393ff4e31000cb48ba9b6009e670ac540851
AnthonyJHizon <AnthonyJHizon714@gmail.com>
2024-05-30 22:18:59 -0700

    bug fix when encountering deprecated item

--- test/intToGold.test.js
+++ test/intToGold.test.js
@@ -2,7 +2,7 @@ import floatToGold from '../utils/formatData/props/floatToGold';
 
 test('convert float value to gold/silver/copper representation', () => {
 	expect(floatToGold(999.9999)).toBe('999g 99s 99c');
-	expect(floatToGold(99.9909)).toBe('99g 99s 9c');
+	expect(floatToGold(99.9909)).toBe('99g 99s 09c');
 	expect(floatToGold(9.0909)).toBe('9g 09s 09c');
 	expect(floatToGold(0.0909)).toBe('9s 09c');
 	expect(floatToGold(0.0009)).toBe('9c');
